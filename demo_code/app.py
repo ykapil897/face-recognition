0d010cc3381b306dea693f0538742575ae28688c
ykapil897 <144621210+ykapil897@users.noreply.github.com>
2024-04-21 01:22:47 +0530

    Update app.py

--- demo_code/app.py
+++ demo_code/app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
 import joblib
-from all_func import HOGFeatureExtractor, resize_images, convert_to_grayscale, read_labels_from_file
+from all_func import prepare_data, resize_images, convert_to_grayscale, read_labels_from_file
 import requests
+from flask_cors import CORS
 from PIL import Image
 from io import BytesIO
 import numpy as np
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +15,7 @@ CORS(app)
 # Load the pre-trained model
 best_model = joblib.load('best_svm_model_hog_pca.pkl')
 pca = joblib.load('pca_hog.pkl')
+
 @app.route('/predict', methods=['POST'])
 def predict():
     image = None
@@ -26,23 +30,30 @@ def predict():
         image = Image.open(BytesIO(response.content))
     else:
         return jsonify({'error': 'No image provided'}), 400
-
+    
     if isinstance(image, np.ndarray):
         print("ye it is numpy arrya")
     else : 
         print("no it is not numpy array")
-    print(image)  
+    print(image)
     image = resize_images(image)
+    print(image)
+    print(image.shape)
     image = convert_to_grayscale(image)
-    hog_extractor = HOGFeatureExtractor(orientations=9, pixels_per_cell=(8, 8), cells_per_block=(2, 2))
-    image = hog_extractor.prepare_data(image)
-    image = pca.transform(image)
-
+    # plt.imshow(image)
+    image, image_data = prepare_data(image)
+    # plt.imshow(image_data)
+    image = pca.transform([image])
+    print(image.shape)
     # Make prediction
     prediction = best_model.predict(image) 
-    prediction = read_labels_from_file("names_labels.txt", prediction)
-
-    return jsonify({'prediction': prediction.tolist()}), 200
+    print(type(prediction))
+    print(prediction.shape)
+    print(prediction)
+    prediction = read_labels_from_file("names_labels.txt", prediction[0])
+    print(prediction)
+    print(jsonify({'prediction': prediction}))
+    return jsonify({'prediction': prediction}), 200
 
 @app.errorhandler(500)
 def internal_server_error(error):
@@ -50,3 +61,4 @@ def internal_server_error(error):
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+

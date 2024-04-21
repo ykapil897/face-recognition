7ab347f5dacf6803b9b191295e309526c3047c27
ykapil897 <144621210+ykapil897@users.noreply.github.com>
2024-04-21 12:46:23 +0530

    Update app.py

--- demo_code/app.py
+++ demo_code/app.py
@@ -7,7 +7,6 @@ from PIL import Image
 from io import BytesIO
 import numpy as np
 from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 CORS(app)
@@ -30,28 +29,18 @@ def predict():
         image = Image.open(BytesIO(response.content))
     else:
         return jsonify({'error': 'No image provided'}), 400
-    
-    if isinstance(image, np.ndarray):
-        print("ye it is numpy arrya")
-    else : 
-        print("no it is not numpy array")
-    print(image)
+
     image = resize_images(image)
-    print(image)
-    print(image.shape)
+
     image = convert_to_grayscale(image)
-    # plt.imshow(image)
+
     image, image_data = prepare_data(image)
-    # plt.imshow(image_data)
+
     image = pca.transform([image])
-    print(image.shape)
     # Make prediction
     prediction = best_model.predict(image) 
-    print(type(prediction))
-    print(prediction.shape)
-    print(prediction)
+
     prediction = read_labels_from_file("names_labels.txt", prediction[0])
-    print(prediction)
     print(jsonify({'prediction': prediction}))
     return jsonify({'prediction': prediction}), 200
 
@@ -61,4 +50,5 @@ def internal_server_error(error):
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
+
 
